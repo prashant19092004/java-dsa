a860386ec4b757153a3c562f734e1bd477798d66
Prashant Kumar <prashantkumargupta998@gmail.com>
2025-03-17 00:14:34 +0530

    postorder traversal done

--- Tree/BinaryTreeB.java
+++ Tree/BinaryTreeB.java
@@ -49,7 +49,16 @@ public class BinaryTreeB{
             inorder(root.right);
         }
 
+        public void postorder(Node root){
 
+            if(root == null){
+                return;
+            }
+
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
     public static void main(String args[]){
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
